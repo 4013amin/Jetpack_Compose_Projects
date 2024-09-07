7389319962f2766fd0e5bf405a91624ee94a12f2
Your Name <your_email@example.com>
2024-09-07 19:35:57 +0330

    Create notifications for show

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:21.0.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
     implementation("com.airbnb.android:lottie-compose:6.5.2")
+    implementation("org.webrtc:google-webrtc:1.0.32006")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
