7adda3dd1b6f718fa3f8747d63d8d815105dc038
delta <amin138400138400@gmail.com>
2024-11-11 20:46:53 +0330

    Tamrin

--- app/src/main/java/com/example/jetpackshop/NewProjectForMe/data/utils/RetrodiInctanse.kt
+++ app/src/main/java/com/example/jetpackshop/NewProjectForMe/data/utils/RetrodiInctanse.kt
@@ -8,5 +8,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrodiInctanse {
 
+    const val baseurl = "http://"
+
+    val api: Apis by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseurl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Apis::class.java)
+    }
 
 }
