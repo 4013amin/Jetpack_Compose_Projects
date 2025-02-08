10dbb5fbf32697be0ea579434813d3de4af9a37e
delta <amin138400138400@gmail.com>
2025-02-08 20:49:27 +0330

    Tamrin in page

--- app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
+++ app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
@@ -8,11 +8,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -22,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
@@ -50,6 +58,11 @@ fun PageMain() {
         var ShowDialog by remember {
             mutableStateOf(false)
         }
+
+        var checked by remember {
+            mutableStateOf(false)
+        }
+
         Box(
             modifier = androidx.compose.ui.Modifier.fillMaxWidth(),
             contentAlignment = Alignment.Center,
@@ -65,6 +78,27 @@ fun PageMain() {
                 onDismissButton = { ShowDialog = false }
             )
         }
+
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = androidx.compose.ui.Modifier.padding(15.dp)
+        ) {
+            Text(text = "This a Check Box")
+            Checkbox(checked = checked, onCheckedChange = { checked = it })
+        }
+
+        ElevatedButton(
+            onClick = { ShowDialog = true },
+            modifier = androidx.compose.ui.Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            enabled = checked
+        ) {
+            Text("Elevated")
+        }
+
     }
 }
 
