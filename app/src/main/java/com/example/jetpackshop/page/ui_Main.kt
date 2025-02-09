1307df80441c7da6f9d5d07213f98a0fe044f0c0
delta <amin138400138400@gmail.com>
2025-02-09 16:12:39 +0330

    Tamrin in page

--- app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
+++ app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
@@ -5,8 +5,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +18,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
@@ -27,10 +33,13 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
@@ -41,7 +50,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +62,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
+import com.example.jetpackshop.R
 import com.example.jetpackshop.Websocket.ui.MainNavigation
 import com.example.jetpackshop.ui.theme.JetPackShopTheme
 import java.lang.reflect.Modifier
@@ -153,6 +167,17 @@ fun PreviewMainPage() {
     var ShowDialog by remember {
         mutableStateOf(false)
     }
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    val scroller = rememberScrollState()
+    var passwordVisible by remember {
+        mutableStateOf(false)
+    }
+
     Scaffold(
         modifier = androidx.compose.ui.Modifier
             .fillMaxSize()
@@ -195,17 +220,56 @@ fun PreviewMainPage() {
             }) {
                 Icon(Icons.Filled.Person, contentDescription = null)
             }
+        },
+        content = {
+            Column(
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .background(color = Color.White)
+                    .verticalScroll(scroller),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.banner_image),
+                    contentDescription = null
+                )
+
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    modifier = androidx.compose.ui.Modifier.padding(15.dp),
+                    placeholder = {
+                        Text(text = "Username")
+                    },
+                    isError = true
+                )
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    modifier = androidx.compose.ui.Modifier.padding(15.dp),
+                    placeholder = {
+                        Text(text = "password")
+                    },
+                    isError = false,
+                    singleLine = true,
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        val image =
+                            if (passwordVisible) Icons.Filled.Person else Icons.Filled.ArrowDropDown
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(
+                                imageVector = image,
+                                contentDescription = "Toggle Password Visibility"
+                            )
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+            }
         }
-    ) {
-        Column(
-            modifier = androidx.compose.ui.Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(5.dp)
-        ) {
-            
-        }
-    }
+    )
 
     if (ShowDialog) {
         ShowDialog(
