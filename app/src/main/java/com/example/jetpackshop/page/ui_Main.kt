b11a965a21d42032264a4fbe5b523246e4c8180f
delta <amin138400138400@gmail.com>
2025-02-09 15:38:05 +0330

    Tamrin in page

--- app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
+++ app/src/main/java/com/example/jetpackshop/page/ui_Main.kt
@@ -1,9 +1,11 @@
 package com.example.jetpackshop.page
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,21 +17,35 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
@@ -39,11 +55,12 @@ import java.lang.reflect.Modifier
 
 
 class ui_Main : FragmentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetPackShopTheme {
-                PageMain()
+                PreviewMainPage()
             }
         }
     }
@@ -129,9 +146,85 @@ fun ShowDialog(
     )
 }
 
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun PreviewMainPage() {
+    var ShowDialog by remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        modifier = androidx.compose.ui.Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    Icon(Icons.Filled.MoreVert, contentDescription = null)
+                },
+                title = {
+                    Text(
+                        text = "This is Top Bar",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF80A4EC),
+                )
+            )
+        },
 
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = androidx.compose.ui.Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                ShowDialog = true
+            }) {
+                Icon(Icons.Filled.Person, contentDescription = null)
+            }
+        }
+    ) {
+        Column(
+            modifier = androidx.compose.ui.Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(5.dp)
+        ) {
+            
+        }
+    }
+
+    if (ShowDialog) {
+        ShowDialog(
+            onDismissRequest = { ShowDialog = false },
+            confirmButton = { ShowDialog = false },
+            onDismissButton = { ShowDialog = false }
+        )
+    }
+}
+
+@Composable
+fun ProductItem() {
+
+}
+
+
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 private fun ShowMAinPge() {
-    PageMain()
+    PreviewMainPage()
 }
