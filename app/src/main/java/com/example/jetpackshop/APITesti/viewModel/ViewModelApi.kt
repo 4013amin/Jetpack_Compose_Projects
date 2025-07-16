b803f2c83ec00f39724b718d150d4a14449e52e3
delta <amin138400138400@gmail.com>
2025-07-16 11:38:08 +0330

    Add New Package for API Test

---
+++ app/src/main/java/com/example/jetpackshop/APITesti/viewModel/ViewModelApi.kt
@@ -0,0 +1,32 @@
+package com.example.jetpackshop.APITesti.viewModel
+
+import android.app.Application
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.jetpackshop.APITesti.data.models.OTPModel
+import com.example.jetpackshop.APITesti.data.utils.Utils_APITest
+import kotlinx.coroutines.launch
+import okio.IOException
+
+class ViewModelApi(application: Application) : AndroidViewModel(application) {
+
+    var data = mutableStateOf<OTPModel?>(null)
+
+    fun sendOTPReqest(phone_number: String) {
+        viewModelScope.launch {
+            val response = try {
+                Utils_APITest.API.OTPRegister(phone_number)
+            } catch (e: IOException) {
+                return@launch
+            } catch (e: HttpException) {
+                return@launch
+            }
+
+            if (response.isSuccessful && response.body() != null) {
+                data.value = response.body()!!
+            }
+        }
+    }
+}
